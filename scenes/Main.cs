ac9b9028301db8a119dc65d4d1d224bcf15367dd
Judson Hester <judsonhester01@gmail.com>
2026-07-19 08:39:23 -0500

    Code Cleanup
    Removed redundancies and modified code so hoveredGridCell is the source of truth.

--- scenes/Main.cs
+++ scenes/Main.cs
@@ -1,10 +1,9 @@
 using Godot;
-using System;
 using System.Collections.Generic;
 
 namespace Game;
 
-public partial class Main : Node2D
+public partial class Main : Node
 {
 
 	private Sprite2D cursor;
@@ -16,7 +15,6 @@ public partial class Main : Node2D
 	private HashSet<Vector2> occupiedCells = new();
 
 
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		buildingScene = GD.Load<PackedScene>("res://scenes/building/Building.tscn");
@@ -31,14 +29,13 @@ public partial class Main : Node2D
 
 	public override void _UnhandledInput(InputEvent evt)
 	{
-		if (cursor.Visible && evt.IsActionPressed("left_click") && !occupiedCells.Contains(GetMouseGridCellPosition()))
+		if (hoveredGridCell.HasValue && evt.IsActionPressed("left_click") && !occupiedCells.Contains(hoveredGridCell.Value))
 		{
-			PlaceBuildingAtMousePosition();
+			PlaceBuildingAtHoveredCellPosition();
 			cursor.Visible = false;
 		}
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 		var gridPosition = GetMouseGridCellPosition();
@@ -52,20 +49,21 @@ public partial class Main : Node2D
 
 	private Vector2 GetMouseGridCellPosition()
 	{
-		var mousePosition = GetGlobalMousePosition();
+		var mousePosition = highlightTileMapLayer.GetGlobalMousePosition();
 		var gridPosition = mousePosition / 64;
 		gridPosition = gridPosition.Floor();
 		return gridPosition;
 	}
 
-	private void PlaceBuildingAtMousePosition()
+	private void PlaceBuildingAtHoveredCellPosition()
 	{
+		if (!hoveredGridCell.HasValue) return;
 		var building = buildingScene.Instantiate<Node2D>();
 		AddChild(building);
 
-		var gridPosition = GetMouseGridCellPosition();
-		building.GlobalPosition = gridPosition * 64;
-		occupiedCells.Add(gridPosition);
+		
+		building.GlobalPosition = hoveredGridCell.Value * 64;
+		occupiedCells.Add(hoveredGridCell.Value);
 
 		hoveredGridCell = null;
 		UpdateHighlightTileMapLayer();
