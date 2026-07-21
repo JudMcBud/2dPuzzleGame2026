fe19b7efa4afb920957c0845662f5cf753b5d5cf
Judson Hester <judsonhester01@gmail.com>
2026-07-21 17:02:28 -0500

    Added Recursive Search For Occupied Tiles
    Also added Code Formatter.

--- scenes/Main.cs
+++ scenes/Main.cs
@@ -5,64 +5,64 @@ namespace Game;
 
 public partial class Main : Node
 {
-	private GridManager gridManager;
-	private Sprite2D cursor;
-	private PackedScene buildingScene;
-	private Button placeBuildingButton;
+    private GridManager gridManager;
+    private Sprite2D cursor;
+    private PackedScene buildingScene;
+    private Button placeBuildingButton;
 
-	private Vector2I? hoveredGridCell;
-	
+    private Vector2I? hoveredGridCell;
 
+    public override void _Ready()
+    {
+        buildingScene = GD.Load<PackedScene>("res://scenes/building/Building.tscn");
+        gridManager = GetNode<GridManager>("GridManager");
+        cursor = GetNode<Sprite2D>("Cursor");
+        placeBuildingButton = GetNode<Button>("PlaceBuildingButton");
 
-	public override void _Ready()
-	{
-		buildingScene = GD.Load<PackedScene>("res://scenes/building/Building.tscn");
-		gridManager = GetNode<GridManager>("GridManager");
-		cursor = GetNode<Sprite2D>("Cursor");
-		placeBuildingButton = GetNode<Button>("PlaceBuildingButton");
+        cursor.Visible = false;
 
-		cursor.Visible = false;
+        placeBuildingButton.Pressed += OnButtonPressed;
+    }
 
-		placeBuildingButton.Pressed += OnButtonPressed;
-	}
+    public override void _UnhandledInput(InputEvent evt)
+    {
+        if (
+            hoveredGridCell.HasValue
+            && evt.IsActionPressed("left_click")
+            && gridManager.IsTilePositionBuildable(hoveredGridCell.Value)
+        )
+        {
+            PlaceBuildingAtHoveredCellPosition();
+            cursor.Visible = false;
+        }
+    }
 
-	public override void _UnhandledInput(InputEvent evt)
-	{
-		if (hoveredGridCell.HasValue && evt.IsActionPressed("left_click") && gridManager.IsTilePositionBuildable(hoveredGridCell.Value))
-		{
-			PlaceBuildingAtHoveredCellPosition();
-			cursor.Visible = false;
-		}
-	}
+    public override void _Process(double delta)
+    {
+        var gridPosition = gridManager.GetMouseGridCellPosition();
+        cursor.GlobalPosition = gridPosition * 64;
+        if (cursor.Visible && (!hoveredGridCell.HasValue || hoveredGridCell.Value != gridPosition))
+        {
+            hoveredGridCell = gridPosition;
+            gridManager.HighlightExpandedBuildableTiles(hoveredGridCell.Value, 3);
+        }
+    }
 
-	public override void _Process(double delta)
-	{
-		var gridPosition = gridManager.GetMouseGridCellPosition();
-		cursor.GlobalPosition = gridPosition * 64;
-		if (cursor.Visible && (!hoveredGridCell.HasValue || hoveredGridCell.Value != gridPosition))
-		{
-			hoveredGridCell = gridPosition;
-			gridManager.HighlightExpandedBuildableTiles(hoveredGridCell.Value, 3);
-		}
-	}
+    private void PlaceBuildingAtHoveredCellPosition()
+    {
+        if (!hoveredGridCell.HasValue)
+            return;
+        var building = buildingScene.Instantiate<Node2D>();
+        building.GlobalPosition = hoveredGridCell.Value * 64;
+        AddChild(building);
 
-	private void PlaceBuildingAtHoveredCellPosition()
-	{
-		if (!hoveredGridCell.HasValue) return;
-		var building = buildingScene.Instantiate<Node2D>();
-		building.GlobalPosition = hoveredGridCell.Value * 64;
-		AddChild(building);
+        hoveredGridCell = null;
+        gridManager.ClearHighlightedTiles();
+    }
 
-		
-
-		hoveredGridCell = null;
-		gridManager.ClearHighlightedTiles();
-
-	}
-
-	private void OnButtonPressed()
-	{
-		cursor.Visible = true;
-		gridManager.HighlightBuildableTiles();
-	}
+    private void OnButtonPressed()
+    {
+        cursor.Visible = true;
+        gridManager.HighlightBuildableTiles();
+    }
 }
--- scenes/component/BuildingComponent.cs
+++ scenes/component/BuildingComponent.cs
@@ -6,27 +6,27 @@ namespace Game.Component;
 
 public partial class BuildingComponent : Node2D
 {
-	[Export]
-	public int BuildableRadius {get; private set;}
+    [Export]
+    public int BuildableRadius { get; private set; }
 
-	public override void _Ready()
-	{
-		// When the building comes into existence, it emits the signal of "BuildingPlaced" through the GameEvents singleton,
-		// which is listened for by the GridManager, which then calls it's own OnBuildingPlaced() method. So the flow looks
-		// like this:
-		// Place Building in Game -> Building is created -> Building Emits Signal -> GameEvents hears signal
-		// -> GameEvents lets subscribers know -> GridManger hears signal -> GridManager executes the logic for when a
-		// building is placed.
-		AddToGroup(nameof(BuildingComponent));
-		// Wrapping the Signal Emission in a Callable allows for .CallDeferred(), which says to run the function at the end
-		// of the frame, after everything else.
-		Callable.From(() => GameEvents.EmitBuildingPlaced(this)).CallDeferred();
-	}
+    public override void _Ready()
+    {
+        // When the building comes into existence, it emits the signal of "BuildingPlaced" through the GameEvents singleton,
+        // which is listened for by the GridManager, which then calls it's own OnBuildingPlaced() method. So the flow looks
+        // like this:
+        // Place Building in Game -> Building is created -> Building Emits Signal -> GameEvents hears signal
+        // -> GameEvents lets subscribers know -> GridManger hears signal -> GridManager executes the logic for when a
+        // building is placed.
+        AddToGroup(nameof(BuildingComponent));
+        // Wrapping the Signal Emission in a Callable allows for .CallDeferred(), which says to run the function at the end
+        // of the frame, after everything else.
+        Callable.From(() => GameEvents.EmitBuildingPlaced(this)).CallDeferred();
+    }
 
-	public Vector2I GetGridCellPosition()
-	{
-		var gridPosition = GlobalPosition / 64;
-		gridPosition = gridPosition.Floor();
-		return new Vector2I((int)gridPosition.X, (int)gridPosition.Y);
-	}
+    public Vector2I GetGridCellPosition()
+    {
+        var gridPosition = GlobalPosition / 64;
+        gridPosition = gridPosition.Floor();
+        return new Vector2I((int)gridPosition.X, (int)gridPosition.Y);
+    }
 }
