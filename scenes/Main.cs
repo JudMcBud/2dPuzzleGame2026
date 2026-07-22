045a1c601d550171094862c1b8780aec5afe8abe
Judson Hester <judsonhester01@gmail.com>
2026-07-22 16:37:26 -0500

    Added Tracking For Collected Resources

--- scenes/Main.cs
+++ scenes/Main.cs
@@ -32,6 +32,8 @@ public partial class Main : Node
 
         placeTowerButton.Pressed += OnPlaceTowerButtonPressed;
         placeVillageButton.Pressed += OnPlaceVillageButtonPressed;
+
+        gridManager.ResourceTilesUpdated += OnResourceTilesUpdated;
     }
 
     public override void _UnhandledInput(InputEvent evt)
@@ -95,5 +97,10 @@ public partial class Main : Node
         toPlaceBuildingResource = villageResource;
         cursor.Visible = true;
         gridManager.HighlightBuildableTiles();
+    }
+
+    private void OnResourceTilesUpdated(int resourceCount)
+    {
+        GD.Print(resourceCount);
     }
 }
--- scenes/manager/GridManager.cs
+++ scenes/manager/GridManager.cs
@@ -11,7 +11,12 @@ public partial class GridManager : Node
 {
     private const string IS_BUILDABLE = "is_buildable";
     private const string IS_WOOD = "is_wood";
+
+    [Signal]
+    public delegate void ResourceTilesUpdatedEventHandler(int collectedTiles);
+
     private HashSet<Vector2I> validBuildableTiles = new();
+    private HashSet<Vector2I> collectedResourceTiles = new();
 
     [Export]
     private TileMapLayer highlightTileMapLayer;
@@ -119,6 +124,21 @@ public partial class GridManager : Node
         validBuildableTiles.ExceptWith(GetOccupiedTiles());
     }
 
+    private void UpdateCollectedResourceTiles(BuildingComponent buildingComponent)
+    {
+        var rootCell = buildingComponent.GetGridCellPosition();
+        var resourceTiles = GetResourceTilesInRadius(
+            rootCell,
+            buildingComponent.BuildingResource.ResourceRadius
+        );
+
+        var oldResourceTileCount = collectedResourceTiles.Count();
+        collectedResourceTiles.UnionWith(resourceTiles);
+
+        if (oldResourceTileCount != collectedResourceTiles.Count())
+            EmitSignal(SignalName.ResourceTilesUpdated, collectedResourceTiles.Count);
+    }
+
     private List<Vector2I> GetTilesInRadius(
         Vector2I rootCell,
         int radius,
@@ -175,5 +195,6 @@ public partial class GridManager : Node
     private void OnBuildingPlaced(BuildingComponent buildingComponent)
     {
         UpdateValidBuildableTiles(buildingComponent);
+        UpdateCollectedResourceTiles(buildingComponent);
     }
 }
